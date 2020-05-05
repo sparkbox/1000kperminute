00d5b120ef48ca9d87929a45e89885483bf6cdc9
Ryan Cromwell <ryan@heysparkbox.com>
2020-05-05 00:43:31 -0400

    helps if you ensure the insert works

--- handlers/captureForGoodMeasure.js
+++ handlers/captureForGoodMeasure.js
@@ -3,7 +3,7 @@ const knex = require('../knex')
 module.exports = function captureForGoodMeasure(request, response) {
   const something = request.body.something
 
-  knex('last_recorded_something').insert({ something })
-
-  response.status(200).end()
+  return knex('last_recorded_something')
+    .insert({ something })
+    .then(() => response.status(200).end())
 }
